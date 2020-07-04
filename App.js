c5c56d7b23655eb8b83dd413a895a62232addb94
Patryk Ptak <ptaszqq@gmail.com>
2020-07-04 16:15:42 +0100

    react-navigation init

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import { StyleSheet, Text, View } from 'react-native';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View>
+        <Stack.Navigator>
+          <Stack.Screen
+            name='Home'
+            component={Home}
+            options={{ title: 'Comic Reader' }}
+          />
+          <Stack.Screen name='Profile' component={Profile} />
+        </Stack.Navigator>
+      </View>
+    </NavigationContainer>
   );
 }
 
